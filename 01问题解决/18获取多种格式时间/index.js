264426b56c166e4181df4f0bb9a160397492a336
harrison <lsfpositivee@163.com>
2021-11-28 17:27:36 +0800

    js获取时间

---
+++ 01问题解决/18获取多种格式时间/index.js
@@ -0,0 +1,20 @@
+
+const getNowTime = () => {
+    let dateTime
+    let yy = new Date().getFullYear()
+    let mm = new Date().getMonth() + 1
+    let dd = new Date().getDate()
+    let hh = new Date().getHours()
+    let mf = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes()
+        :
+        new Date().getMinutes()
+    let ss = new Date().getSeconds() < 10 ? '0' + new Date().getSeconds()
+        :
+        new Date().getSeconds()
+    // dateTime = yy + '-' + mm + '-' + dd + ' ' + hh + ':' + mf + ':' + ss
+    dateTime = yy.toString() + mm + dd+  hh +  mf +  ss
+    console.log(dateTime)
+    return dateTime
+}
+
+let time = getNowTime()
